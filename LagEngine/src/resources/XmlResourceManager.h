003e4514d96f067fba21638f6c0b41fa70424b5b
Bruno Lourenço <bmlourenco@gmail.com>
2018-03-09 17:49:49 +0000

    Some cleanups.

--- LagEngine/src/resources/XmlResourceManager.h
+++ LagEngine/src/resources/XmlResourceManager.h
@@ -13,39 +13,13 @@ namespace Lag
 	class XmlResourceBuilder : public IManagedObjectBuilder<std::string, V>
 	{
 	public:
-		XmlResourceBuilder(const std::string &tagToParse, const TiXmlDocument &resourcesXml, const std::string &resourceFolderPath) :
-			tagToParse(tagToParse),
-			resourcesXml(resourcesXml),
-			resourceFolderPath(resourceFolderPath)
-		{
-			// Compile-time sanity check
-			static_assert(std::is_base_of<XmlResource, V>::value, "Creating a XmlResourceBuilder of Vs not derived from XmlResource");
-		}
-
-		virtual V* build(const std::string &name) const override
-		{
-			const TiXmlElement* element = findResourceNameOnXml(name);
-			if (element != nullptr)
-				return parseAndCreate(name, *element);
-			else
-				return nullptr;
-		}
+		XmlResourceBuilder(const std::string &tagToParse, const TiXmlDocument &resourcesXml, const std::string &resourceFolderPath);
+		virtual V* build(const std::string &name) const override;
 
 	protected:
-		const std::string parseFileAttribute(const TiXmlElement &element) const
-		{
-			const char* file = element.Attribute("file");
-			if (!file)
-			{
-				LogManager::getInstance().log(LAG_LOG_TYPE_ERROR, LAG_LOG_VERBOSITY_NORMAL, "XmlResourceBuilder",
-					"A <" + tagToParse + "> element on the Resources file does not contain the required attribute <file>.");
-				return "";
-			}
-			return file;
-		}
-
 		const TiXmlElement* findResourceNameOnXml(const std::string &name) const;
 		virtual V* parseAndCreate(const std::string &name, const TiXmlElement &element) const = 0;
+		const std::string parseFileAttribute(const TiXmlElement &element) const;
 
 		const TiXmlDocument &resourcesXml;
 		std::string tagToParse;
@@ -64,9 +38,29 @@ namespace Lag
 	};
 
 
+
 	/*
 	* DEFINITION HERE 'CAUSE C++ TEMPLATE COMPILATION...
 	*/
+	template<class V>
+	XmlResourceBuilder<V>::XmlResourceBuilder(const std::string &tagToParse, const TiXmlDocument &resourcesXml, const std::string &resourceFolderPath) :
+		tagToParse(tagToParse),
+		resourcesXml(resourcesXml),
+		resourceFolderPath(resourceFolderPath)
+	{
+		// Compile-time sanity check
+		static_assert(std::is_base_of<XmlResource, V>::value, "Creating a XmlResourceBuilder of Vs not derived from XmlResource");
+	}
+
+	template<class V>
+	V* XmlResourceBuilder<V>::build(const std::string &name) const
+	{
+		const TiXmlElement* element = findResourceNameOnXml(name);
+		if (element != nullptr)
+			return parseAndCreate(name, *element);
+		else
+			return nullptr;
+	}
 
 	template<class V>
 	const TiXmlElement* XmlResourceBuilder<V>::findResourceNameOnXml(const std::string &name) const
@@ -80,6 +74,20 @@ namespace Lag
 		}
 		return nullptr;
 	}
+
+	template<class V>
+	const std::string XmlResourceBuilder<V>::parseFileAttribute(const TiXmlElement &element) const
+	{
+		const char* file = element.Attribute("file");
+		if (!file)
+		{
+			LogManager::getInstance().log(LAG_LOG_TYPE_ERROR, LAG_LOG_VERBOSITY_NORMAL, "XmlResourceBuilder",
+				"A <" + tagToParse + "> element on the Resources file does not contain the required attribute <file>.");
+			return "";
+		}
+		return file;
+	}
+
 
 	template<class V>
 	XmlResourceManager<V>::XmlResourceManager(const std::string &name, XmlResourceBuilder<V> *builder) :
